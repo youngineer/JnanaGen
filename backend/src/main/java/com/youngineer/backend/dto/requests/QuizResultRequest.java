7c57476989d5d54ed93f175c10f3b0308bd5fc78
youngineer <youngineer@protonmail.com>
2025-07-15 23:03:35 +0530

    feat: implement quiz score calculation and result storage
    Evaluate user quiz attempts by comparing selected options with correct answers
    from the Question table. Calculate total score, store the result in the database,
    and return the score along with correct answers for each question.

--- backend/src/main/java/com/youngineer/backend/dto/requests/QuizResultRequest.java
+++ backend/src/main/java/com/youngineer/backend/dto/requests/QuizResultRequest.java
@@ -2,10 +2,17 @@ package com.youngineer.backend.dto.requests;
 
 import org.springframework.lang.NonNull;
 
+import java.util.HashMap;
 import java.util.List;
 
 public record UserQuizResultRequest(
         @NonNull
-        List<String> answers
+        Long quizId,
+
+        @NonNull
+        HashMap<Long, Long> questionOptionMap,
+
+        @NonNull
+        List<String> answerOptionList
 ) {
 }
