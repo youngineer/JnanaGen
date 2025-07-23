4ca52fc93f3d4fff413978666a8f1094b2023f6b
youngineer <youngineer@protonmail.com>
2025-07-23 22:03:01 +0530

    feat: redirect unattempted quizzes to attempt page and add submit logic
    Fix issue where users could view their created quizzes without attempting.
    Redirect unattempted quizzes to the quiz attempt page instead of results.
    Implement 'Submit Quiz' functionality to generate the required payload
    for score evaluation.

--- frontend/src/services/quizServices.ts
+++ frontend/src/services/quizServices.ts
@@ -1,10 +1,10 @@
+
 import { BASE_QUIZ_URL, MY_HEADER } from "../utils/constants"
-import type { AttemptedQuizInfo, QuizDetails, QuizInfoProps, QuizSpecification } from "../utils/interfaces";
+import type { AttemptedQuizInfo, Quiz, QuizDetails, QuizInfoProps, QuizSpecification, ScorePayload } from "../utils/interfaces";
 
 
 export const generateQuiz = async(userInput: QuizSpecification): Promise<void> => {
     const url = BASE_QUIZ_URL + "/generateQuiz";
-    console.log(userInput);
     const request = new Request(url, {
         method: "POST",
         body: JSON.stringify(userInput),
@@ -22,6 +22,35 @@ export const generateQuiz = async(userInput: QuizSpecification): Promise<void> =
                 data?.message || "Error generating the quiz"
             });
         } else {
+            return data?.content;
+        }
+    } catch (error) {
+        console.error('Network error. Please check your connection.');
+        return Promise.reject('Network error. Please check your connection.');
+    }
+}
+
+
+export const loadQuiz = async(id: string): Promise<Quiz> => {
+    const url = BASE_QUIZ_URL + "/loadQuiz/" + id;
+    console.log(url)
+    const request = new Request(url, {
+        method: "GET",
+        headers: MY_HEADER,
+        credentials: "include"
+    });
+
+    try {
+        const response = await fetch(request);
+        const data = await response?.json();
+
+        if(!response.ok) {
+            console.error(data?.message || "Error generating the quiz")
+            return Promise.reject(() => {
+                data?.message || "Error generating the quiz"
+            });
+        } else {
+            console.log(data?.content)
             return data?.content;
         }
     } catch (error) {
@@ -90,3 +119,55 @@ export const fetchUserQuiz = async(path: string): Promise<AttemptedQuizInfo> => 
     }
 }
 
+
+export const getRedirectionPath = async(quizId: string) => {
+    const url = BASE_QUIZ_URL + "/getRedirectionPath/" + quizId;
+    const request = new Request(url, {
+        method: "GET",
+        headers: MY_HEADER,
+        credentials: "include"
+    });
+
+    try {
+        const response = await fetch(request);
+        const data = await response?.json();
+
+        if(!response.ok) {
+            return Promise.reject(() => {
+                data?.message || "Error making the api call";
+            });
+        } else {
+            return data?.content;
+        }
+    } catch (error) {
+        console.error('Network error. Please check your connection:' , error);
+        return Promise.reject('Network error. Please check your connection:' + error);
+    }
+}
+
+
+export const calculateScore = async(payload: ScorePayload): Promise<string> => {
+    const url = BASE_QUIZ_URL + "/calculateScore";
+    const request = new Request(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: MY_HEADER,
+        credentials: "include"
+    });
+
+    try {
+        const response = await fetch(request);
+        const data = await response?.json();
+
+        if(!response.ok) {
+            return Promise.reject(() => {
+                data?.message || "Error calculating the results";
+            });
+        } else {
+            return data?.content;
+        }
+    } catch (error) {
+        console.error('Network error. Please check your connection: ' , error);
+        return Promise.reject('Network error. Please check your connection: ' + error);
+    }
+}
